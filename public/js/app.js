0167c8cca4c83e3c462ada96167faf7efb7d489a
SamAlfa <sam.tunk@gmail.com>
2016-01-05 23:06:41 +0700

    #2. commit <setting up app and roues #EP1>

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+/*This is the main file where angular is defined*/
+var myApp = angular.module('myApp', ['ngRoute', 'ngCookies']);
+
+myApp.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'templates/users/login.html',
+            controller: 'userController'
+        });
+
+        $routeProvider.when('/dashboard', {
+            templateUrl: 'templates/users/dashboard.html',
+            controller: 'userController'//,
+            //authenticated: true
+        });
+
+        $routeProvider.when('/logout', {
+            templateUrl: 'templates/users/logout.html',
+            controller: 'userController'//,
+            //authenticated: true
+        });
+
+        $routeProvider.otherwise('/');
+    }
+]);
+//# sourceMappingURL=app.js.map
---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html ng-app="myApp">
+<head>
+	<title>Gallery application in Angular JS</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+</head>
+<body>
+	<div class="container">
+		<div ng-view></div>
+	</div>
+	<script type="text/javascript" src="{{asset('bower_components/angular/angular.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('bower_components/angular-route/angular-route.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('bower_components/angular-cookies/angular-cookies.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/controllers.js')}}"></script>
+</body>
+</html>
